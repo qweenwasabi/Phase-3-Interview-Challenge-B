45b1f361b570f1cea4bb145154ab3bceb64b8a34
Jacklyne Bouier <jacky_bouier@yahoo.com>
2017-08-28 23:15:25 -0700

    modal, anchor , and cart count added

---
+++ Part-3/grocer.js
@@ -0,0 +1,58 @@
+var cart = document.getElementById("cart-item-count");
+var cartNumber = cart.textContent;
+var sliceNumber = cartNumber.slice(1,2);
+
+
+
+var plus1 = Number(sliceNumber);
+
+function addToCartCount(){
+   plus1++;
+   cart.textContent = "("+ plus1 + ")"
+  return plus1;
+}
+
+var modal = document.getElementById("popUp");
+var cartButton = document.getElementById("cart-button");
+cartButton.onclick = function() {
+    modal.style.display = "block";
+     cartItemList()
+}
+var exit = document.getElementsByClassName("close")[0];
+exit.onclick = function() {
+    modal.style.display = "none";
+}
+
+var cartItems = []
+var itemsPrice = []
+
+
+function addToCart(item, price) {
+  cartItems.push(item)
+  itemsPrice.push('$' + price)
+}
+
+
+function cartItemList() {
+  document.getElementsByClassName('itemInCart')[0].innerHTML = ''
+  document.getElementsByClassName('ItemPriceInCart')[0].innerHTML = ''
+  var itemInCart = document.getElementsByClassName('itemInCart')[0]
+  var ItemPriceInCart = document.getElementsByClassName('ItemPriceInCart')[0]
+
+  for (var i = 0; i < cartItems.length; i++) {
+    var modalItem = document.createElement('li')
+    var modalPrice = document.createElement('li')
+    modalItem.innerText = cartItems[i]
+    modalPrice.innerText = itemsPrice[i]
+    itemInCart.appendChild(modalItem)
+    ItemPriceInCart.appendChild(modalPrice)
+  }
+}
+
+function clearCart() {
+  plus1 = 0
+  cartItems = []
+  itemsPrice = []
+  cart.innerHTML = `(${plus1})`
+  modal.style.display = 'none'
+}
